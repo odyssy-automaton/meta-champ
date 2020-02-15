172f8560fcf36122395f1ad21458212f546794a4
skuhlmann <samkuhlmann@gmail.com>
2020-02-15 14:42:38 -0700

    Adds formatic and formatic hook

--- src/views/new-crop/NewCrop.js
+++ src/views/new-crop/NewCrop.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const NewCrop = () => (
-  <div className="View">
-    <h1>New Crop Form</h1>
-  </div>
-);
+import useFortmatic from '../../util/UseFortmatic';
+
+const API_KEY = process.env.REACT_APP_FORTMATIC_API_KEY;
+
+const NewCrop = () => {
+  const {
+    accounts,
+    signOut,
+    signIn,
+    isSignedIn,
+    web3Ready,
+    web3IsInitialized,
+  } = useFortmatic(API_KEY);
+
+  console.log('accounts', accounts);
+  console.log('signOut', signOut);
+  console.log('signIn', signIn);
+  console.log('isSignedIn', isSignedIn);
+  console.log('web3Ready', web3Ready);
+  console.log('web3IsInitialized', web3IsInitialized);
+
+  return (
+    <div className="View">
+      {isSignedIn(accounts) ? (
+        <h1>New Crop Form</h1>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
+    </div>
+  );
+};
 
 export default NewCrop;
